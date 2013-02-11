a8b6622e9fbb2d4765ad1399e5b758b76d5bc494
Bernard Blackham <b-gitcommits@largestprime.net>
2013-02-11 19:02:30 +0000

    Fix more places where SubmissionResult must be used.

--- cms/grading/__init__.py
+++ cms/grading/__init__.py
@@ -23,7 +23,7 @@ import os
 import codecs
 
 from cms import logger
-from cms.db.SQLAlchemyAll import SessionGen, Submission
+from cms.db.SQLAlchemyAll import SessionGen, Submission, SubmissionResult
 from cms.grading.Sandbox import Sandbox
 
 
@@ -571,15 +571,15 @@ def task_score(user, task):
                           yet to score.
 
     """
-    def waits_for_score(submission):
+    def waits_for_score(submission_result):
         """Return if submission could be scored but it currently is
         not.
 
-        submission (Submission): the submission to check.
+        submission_result (SubmissionResult): the result to check.
 
         """
-        return submission.compilation_outcome != "fail" and \
-               not submission.scored()
+        return submission_result.compilation_outcome != "fail" and \
+               not submission_result.scored()
 
     # The score of the last submission (if valid, otherwise 0.0).
     last_score = 0.0
@@ -602,17 +602,24 @@ def task_score(user, task):
         # Last score: if the last submission is scored we use that,
         # otherwise we use 0.0 (and mark that the score is partial
         # when the last submission could be scored).
-        if submissions[-1].scored():
-            last_score = submissions[-1].score
-        elif waits_for_score(submissions[-1]):
+        s = submissions[-1]
+        last_submission_result = SubmissionResult.get_from_submission_id(
+            s.id, s.task.active_dataset_version, session)
+
+        if last_submission_result.scored():
+            last_score = last_submission_result.score
+        elif waits_for_score(last_submission_result):
             partial = True
 
         for submission in submissions:
+            sr = SubmissionResult.get_from_submission_id(
+                submission.id, submission.task.active_dataset_version,
+                session)
             if submission.token is not None:
-                if submission.scored():
+                if sr.scored():
                     max_tokened_score = max(max_tokened_score,
-                                            submission.score)
-                elif waits_for_score(submission):
+                                            sr.score)
+                elif waits_for_score(sr):
                     partial = True
 
     return max(last_score, max_tokened_score), partial
